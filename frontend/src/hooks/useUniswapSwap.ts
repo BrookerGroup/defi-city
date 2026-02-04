5396098dc35a52dbb232ef065913f02ed3a0d978
prateep <prateep@brookergroup.com>
2026-02-04 17:37:09 +0700

    - uniswap update

--- frontend/src/hooks/useUniswapSwap.ts
+++ frontend/src/hooks/useUniswapSwap.ts
@@ -189,7 +189,7 @@ export function useUniswapSwap(smartWallet: string | null) {
           const erc20 = new ethers.Interface([
             'function approve(address,uint256) returns (bool)',
           ])
-          const routerIface = new ethers.Interface(ABIS.SWAP_ROUTER_02 as string[])
+          const routerIface = new ethers.Interface([...ABIS.SWAP_ROUTER_02])
           const approveData = erc20.encodeFunctionData('approve', [
             router,
             amountInRaw,
